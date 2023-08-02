a54ec54e377f4541d90cdbddcc48283c4d93c17d
rydnr <github@acm-sl.org>
2023-08-02 20:13:09 +0200

    Initial implementation on top of nbformat

---
+++ pythoneda/shared/code_requests/jupyter/jupyter_code_request.py
@@ -0,0 +1,69 @@
+"""
+pythoneda/shared/code_requests/jupyter/jupyter_code_request.py
+
+This file defines the JupyterCodeRequest class.
+
+Copyright (C) 2023-today rydnr's pythoneda-shared-code-requests/jupyter
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+import nbformat
+from pythoneda.shared.code_requests.code_request import CodeRequest
+from pythoneda.value_object import primary_key_attribute
+
+class JupyterCodeRequest(CodeRequest):
+
+    """
+    Jupyter-based code requests.
+
+    Class name: JupyterCodeRequest
+
+    Responsibilities:
+        - Model code requests using Jupyter notebooks.
+
+    Collaborators:
+        - pythoneda.shared.code_requestscode_request.CodeRequest
+    """
+    def __init__(self):
+        """
+        Creates a new JupyterCodeRequest instance.
+        """
+        super().__init__()
+        self._notebook = nbformat.v4.new_notebook()
+
+    @property
+    @primary_key_attribute
+    def notebook(self):
+        """
+        Retrieves the notebook instance.
+        :return: Such instance.
+        :rtype: nbformat.v4.notebook.Notebook
+        """
+        return self._notebook
+
+    def append(self, pythonCode: str):
+        """
+        Appends a new cell.
+        :param pythonCode: The Python code to add.
+        :type pythonCode: str
+        """
+        self.notebook.cells.append(nbformat.v4.new_code_cell(pythonCode))
+
+    def write(self, file):
+        """
+        Writes the code request to a file.
+        :param file: The file to write.
+        :type file: File
+        """
+        nbformat.write(self.notebook, file)
